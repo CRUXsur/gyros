33d885a6135761e6b8c500c897d03e22024b8320
Marco Torrico <marcojtorrico@gmail.com>
2025-08-24 13:24:06 -0400

    before update

--- backend/src/prestamos/entities/prestamo.entity.ts
+++ backend/src/prestamos/entities/prestamo.entity.ts
@@ -1,4 +1,4 @@
-import{ BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import{ BeforeInsert, BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Prestamo {
@@ -43,6 +43,14 @@ export class Prestamo {
         if(!this.slug){
             this.slug = this.title;
         }
+        this.slug = this.slug
+         .toLowerCase()
+         .replaceAll(' ', '_')
+         .replaceAll("'", '');
+     }
+
+     @BeforeUpdate()
+     checkSlugUpdate(){
         this.slug = this.slug
          .toLowerCase()
          .replaceAll(' ', '_')
